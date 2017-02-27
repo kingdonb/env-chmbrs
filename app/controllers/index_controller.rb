60f72ac4c2774b8bd4f577667c228a7f7e19530d
Kingdon Barrett <kbarret8@nd.edu>
2017-02-26 19:31:23 -0500

    show everything connected to database

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,7 @@
 class IndexController < ApplicationController
   def main
+    @data_count = EnvDatum.all.count
+    @thing1_count = EnvDatum.where(thing_id: 1).count
+    @thing2_count = EnvDatum.where(thing_id: 2).count
   end
 end
