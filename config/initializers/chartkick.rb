256251d268210ffbd9dc2a439395c766cd3027be
Kingdon Barrett <kbarret8@nd.edu>
2017-02-26 23:24:17 -0500

    remove default width

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,9 +1,8 @@
 Chartkick.options = {
   height: "400px",
-  width: "600px",
   colors: ["#b00", "#666"]
 }
 
-Chartkick.options[:html] = '<div id="%{id}" style="height: %{height};width: %{width};">Loading...</div>'
+Chartkick.options[:html] = '<div id="%{id}" style="height: %{height}">Loading...</div>'
 Chartkick.options[:content_for] = :charts_js
 
