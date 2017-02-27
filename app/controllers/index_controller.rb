519d65437eb9e810b471ace9b8d12784767e0859
Kingdon Barrett <kbarret8@nd.edu>
2017-02-26 23:09:52 -0500

    add charts to main index

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,14 @@
 class IndexController < ApplicationController
   def main
     @data_count = EnvDatum.all.count
-    @thing1_count = EnvDatum.where(thing_id: 1).count
-    @thing2_count = EnvDatum.where(thing_id: 2).count
+
+    Time.zone = "Eastern Time (US & Canada)"
+    # where("created_at >= ?", DateTime.now - 10.hours).
+    thing1 = EnvDatum.where(thing_id: 1).
+      where("created_at >= ?", Time.zone.now - 1.day)
+    thing2 = EnvDatum.where(thing_id: 2).
+      where("created_at >= ?", Time.zone.now - 1.day)
+    @thing1_data = thing1.group_by_period(:hour, :created_at, format: "%-l %P").average(:temp_c)
+    @thing2_data = thing2.group_by_period(:hour, :created_at, format: "%-l %P").average(:temp_c)
   end
 end
