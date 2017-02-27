28a8f1b45206b4cbd9c437f22b47b24b025dec41
Kingdon Barrett <kbarret8@nd.edu>
2017-02-26 23:17:13 -0500

    put them on one graph

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,14 +1,8 @@
 class IndexController < ApplicationController
   def main
-    @data_count = EnvDatum.all.count
-
     Time.zone = "Eastern Time (US & Canada)"
-    # where("created_at >= ?", DateTime.now - 10.hours).
-    thing1 = EnvDatum.where(thing_id: 1).
-      where("created_at >= ?", Time.zone.now - 1.day)
-    thing2 = EnvDatum.where(thing_id: 2).
-      where("created_at >= ?", Time.zone.now - 1.day)
-    @thing1_data = thing1.group_by_period(:hour, :created_at, format: "%-l %P").average(:temp_c)
-    @thing2_data = thing2.group_by_period(:hour, :created_at, format: "%-l %P").average(:temp_c)
+
+    @thing_data = EnvDatum.where("created_at >= ?", Time.zone.now - 1.day).group(:thing_id)
+    @data_count = EnvDatum.all.count
   end
 end
