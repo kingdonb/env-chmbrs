4f60287e69a8b3076cf97b45de10b376b8cd9bfb
Kingdon Barrett <kbarrett@metrixmatrix.com>
2017-04-02 17:29:23 +0000

    two weeks!

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -5,7 +5,7 @@ class IndexController < ApplicationController
     @thing_data_short = EnvDatum.joins(:thing).group('things.name').
       where("env_data.created_at >= ?", Time.zone.now - 4.hours)
     @thing_data_long = EnvDatum.joins(:thing).group('things.name').
-      where("env_data.created_at >= ?", Time.zone.now - 1.day)
+      where("env_data.created_at >= ?", Time.zone.now - 2.weeks)
     @data_count = EnvDatum.all.count
   end
 end
