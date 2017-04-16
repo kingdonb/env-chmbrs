6fd4af2d9779256db2a0ef58427301c4ab41c0ac
Kingdon Barrett <kbarret8@nd.edu>
2017-04-16 18:26:33 -0400

    Great idea - average by hour for 2 weeks dataset

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -6,6 +6,9 @@ class IndexController < ApplicationController
       where("env_data.created_at >= ?", Time.zone.now - 4.hours)
     @thing_data_long = EnvDatum.joins(:thing).group('things.name').
       where("env_data.created_at >= ?", Time.zone.now - 1.day)
+
+    @thing_data_vlong = EnvDatum.joins(:thing).group('things.name').
+      where("env_data.created_at >= ?", Time.zone.now - 2.weeks)
     @data_count = EnvDatum.all.count
   end
 end
